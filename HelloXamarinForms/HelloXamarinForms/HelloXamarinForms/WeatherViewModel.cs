7b37adef04e17bbcc3cb62e78b9bad2c1f65239d
Vikas Mittal <vikas.mittal@simplivity.com>
2016-06-07 08:43:56 -0400

    iReferU project

---
+++ HelloXamarinForms/HelloXamarinForms/HelloXamarinForms/WeatherViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloXamarinForms
+{
+    public class WeatherViewModel
+    {
+        public string MainText { get; set; } = "View model page";
+    }
+}
